84fabc54f859e0a0661fa2a49d9cb36e7484c1be
seyadava@microsoft.com <seyadava@microsoft.com>
2018-08-23 11:18:49 -0700

    Added resource group existance check test

--- TestProject/TestSamples.cs
+++ TestProject/TestSamples.cs
@@ -82,5 +82,34 @@ namespace TestProject
             Assert.NotNull(rp);
             Assert.True(String.Equals("registered", rp.RegistrationState, StringComparison.InvariantCultureIgnoreCase));
         }
+
+        [Fact]
+        public async Task ExistanceCheckResourceGroupTest()
+        {
+            const string subscriptionId = "fa9ea22d-a053-4a9e-9e76-d7f71c1359de";
+            const string location = "eastus";
+            var baseUri = new Uri("https://management.azure.com/");
+            string servicePrincipalId = "5acab3e0-d042-49e0-86e1-cca5c52c165b";
+            string servicePrincipalSecret = "683c1b3e-5479-451d-9186-9ee6b5f130b7";
+            string azureEnvironmentResourceId = "https://management.core.windows.net/";
+            string azureEnvironmentTenandId = "72f988bf-86f1-41af-91ab-2d7cd011db47";
+
+            //const string location = "redmond";
+            //var baseUri = new Uri("https://management.redmond.ext-v.masd.stbtest.microsoft.com/");
+
+            var credentials = new CustomLoginCredentials(
+                servicePrincipalId, servicePrincipalSecret, azureEnvironmentResourceId, azureEnvironmentTenandId);
+
+            var resourceController = new ResourcesController(baseUri, credentials, subscriptionId);
+            var rgname = "test-dotnet-rg-3";
+            var rg = await resourceController.CreateResourceGroup(rgname, location);
+
+            Assert.NotNull(rg);
+            Assert.True(String.Equals("Succeeded", rg.Properties.ProvisioningState, StringComparison.InvariantCultureIgnoreCase));
+
+            var rgExistance = await resourceController.CheckResourceGroupExistance(rgname);
+
+            Assert.True(rgExistance);
+        }
     }
 }
