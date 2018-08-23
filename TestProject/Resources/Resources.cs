3ac91951f4a94bff65e71a2395c78a2133e3c2fc
seyadava@microsoft.com <seyadava@microsoft.com>
2018-08-23 08:52:20 -0700

    Add test project

---
+++ TestProject/Resources/Resources.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Authorization;
+using Profile2018ResourceManager = Microsoft.Azure.Management.Profiles.hybrid_2018_03_01.ResourceManager;
+
+namespace Resources
+{
+    public class Resources
+    {
+
+        private static Profile2018ResourceManager.ResourceManagementClient client;
+
+        public static Profile2018ResourceManager.ResourceManagementClient GetClient
+            (Uri baseUri, CustomLoginCredentials credentials, string subscriotionId)
+        {
+            if (client == null)
+            {
+                client = new Profile2018ResourceManager.ResourceManagementClient(baseUri: baseUri, credentials: credentials);
+                client.SubscriptionId = subscriotionId;
+            }
+            return client;
+        }
+
+        public static async Task<Profile2018ResourceManager.Models.ResourceGroup> CreateResourceGroup(string resourceGroupName, string location)
+        {
+            if (client == null)
+            {
+                throw new Exception("Client is not instantiated");
+            }
+            var resourceGroup = await client.ResourceGroups.CreateOrUpdateWithHttpMessagesAsync(
+                resourceGroupName: resourceGroupName,
+                parameters: new Profile2018ResourceManager.Models.ResourceGroup
+                {
+                    Location = location
+                });
+
+            return resourceGroup.Body;
+        }
+    }
+}
